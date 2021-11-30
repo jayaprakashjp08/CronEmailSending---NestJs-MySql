ad9d886e03eb160603e90d6c450cf03c6db46e04
jayaprakash-GitHub08 <79584401+jayaprakash-GitHub08@users.noreply.github.com>
2021-12-01 00:15:04 +0530

    scrapped data from html table successfully and stored

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,31 @@ import { DailyData } from './entities/dailyData.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-
 @Injectable()
 export class AppService {
   constructor(
     @InjectRepository(DailyData)
     private readonly dailyDataRepository: Repository<DailyData>,
   ) {}
-  
+  async storeDetails(data: any) {
+    const dailyData = [];
+    const date = new Date();
+    data.forEach((element) => {
+      dailyData.push({
+        itemName: element['Vegetable_2'],
+        minPrice: element['Min.Rate/kg.'],
+        maxPrice: element['Max.Rate/kg.'],
+        date: date,
+      });
+    });
+    await this.dailyDataRepository
+      .save(dailyData)
+      .then((data) => {
+        Logger.log("Inserted all data's successfully");
+      })
+      .catch((error) => {
+        Logger.log('Error occured while insertion');
+      });
+    console.log('data', dailyData);
+  }
 }
